16a98a6ea762f18d5a425b11027fbfd9aad020cf
p1956 <yishaofang1@163.com>
2019-01-21 12:10:55 +0800

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,4 @@
+#安装SS（系统支持：CentOS，Debian，Ubuntu）
+wget --no-check-certificate -O shadowsocks-go.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
+chmod +x shadowsocks-go.sh
+./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
