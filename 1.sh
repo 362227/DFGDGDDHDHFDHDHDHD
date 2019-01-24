0df621c1bb5e5b9fa2b6e8ea2db8a3bbe3b6cafc
p1956 <yishaofang1@163.com>
2019-01-25 00:47:43 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -923,11 +923,6 @@ service nginx restart
 
 
 
-#安装SS（系统支持：CentOS，Debian，Ubuntu）
-wget --no-check-certificate -O shadowsocks-go.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
-chmod +x shadowsocks-go.sh
-./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
-
 #安装Pip： 
 yum install python-pip 
 
