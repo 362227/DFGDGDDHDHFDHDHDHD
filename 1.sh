0e95cfe1073f4d62d386cb1f0f8194b38cc9916d
p1956 <yishaofang1@163.com>
2019-01-24 21:01:54 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -923,10 +923,6 @@ service nginx restart
 
 
 
-#安装rTorrent
-wget https://github.com/p1956/DFGDGDDHDHFDHDHDHD/raw/master/rTorrent0.9.7CentOS7install.sh && chmod +x rTorrentCentOS7install.sh
-./rTorrentCentOS7install.sh
-
 #安装SS（系统支持：CentOS，Debian，Ubuntu）
 wget --no-check-certificate -O shadowsocks-go.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
 chmod +x shadowsocks-go.sh
