6ae2c9dfb488ecb6d6f90be2462a89f29e838012
p1956 <yishaofang1@163.com>
2019-04-07 10:20:32 +0800

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,56 @@
+#0.安装goflyway
+wget -N --no-check-certificate https://github.com/p1956/DFGDGDDHDHFDHDHDHD/raw/master/goflyway.sh && chmod +x goflyway.sh && bash goflyway.sh
+
+#1.安装transmission 
+wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/91yuncode/master/transmission-centos.sh && bash transmission-centos.sh
+wget https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh --no-check-certificate && bash install-tr-control-cn.sh
+##（注：如要更新新版，执行此命令即可）
+##开机启动：
+chkconfig transmission-daemon on
+
+
+
+# 安装FFMPEG
+cd ~
+wget --no-check-certificate https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.0.3-64bit-static.tar.xz
+tar -xJf ffmpeg-4.0.3-64bit-static.tar.xz
+cd ffmpeg-4.0.3-64bit-static
+cp ffmpeg /usr/bin/ffmpeg
+cd ~
+ffmpeg -version
+if [ $? -eq 0 ];then
+    echo -e "${green} FFMPEG安装成功 ${font}"
+else 
+    echo -e "${red} FFMPEG安装失败 ${font}"
+    exit 1
+fi
+
+
+#安装mediainfo
+yum -y install mediainfo
+
+#安装youtube-dl
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl 
+
+#安装SSR
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+chmod +x shadowsocksR.sh
+./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+
+#安装Pip： 
+yum -y install python-pip 
+
+iptables -F
+iptables -F -t nat
+iptables -X
+iptables -X -t nat
+
+#安装v2ray
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/p1956/DFGDGDDHDHFDHDHDHD/master/V2ray.fun.sh)"
+
+#安装streamlink
+pip install --upgrade streamlink
+
+#安装psmisc
+yum -y install psmisc
