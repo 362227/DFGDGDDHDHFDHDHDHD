9c80cfa7344ace3292ffb872eaf0ef8394109be5
10362227 <10362227@163.com>
2021-09-04 20:11:42 +0800

    Update sw.sh

--- sw.sh
+++ sw.sh
@@ -207,14 +207,14 @@ chmod -R 777 /usr/share/nginx/kodexplorer/
 chmod -R 777 /usr/share/nginx/
 
 #0.安装goflyway
-curl -L  https://github.com/p1956/DFGDGDDHDHFDHDHDHD/raw/master/goflyway.sh --output goflyway.sh  && chmod +x goflyway.sh && bash goflyway.sh
+curl -L https://raw.githubusercontent.com/10362227/DFGDGDDHDHFDHDHDHD/master/goflyway.sh --output goflyway.sh  && chmod +x goflyway.sh && bash goflyway.sh
 #http://kernel.ubuntu.com/~kernel-ppa/mainline/
 
 
 #重启nginx
 #systemctl restart nginx
 
-cat > /etc/nginx/conf.d/kodexplorer.conf <<EOF
+#cat > /etc/nginx/conf.d/kodexplorer.conf <<EOF
 server {
     listen       80;
     server_name  wwww.362227.top;
@@ -235,129 +235,7 @@ server {
 EOF
 
 
-#cat > /etc/nginx/conf/nginx.conf <<EOF
 
-user  root;
-worker_processes  3;
-
-#error_log  logs/error.log;
-#error_log  logs/error.log  notice;
-#error_log  logs/error.log  info;
-
-#pid        logs/nginx.pid;
-
-
-events {
-    worker_connections  4096;
-}
-
-
-
-
-http {
-    include       mime.types;
-    default_type  application/octet-stream;
-
-    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-    #                  '$status $body_bytes_sent "$http_referer" '
-    #                  '"$http_user_agent" "$http_x_forwarded_for"';
-
-    #access_log  logs/access.log  main;
-
-    sendfile        on;
-    #tcp_nopush     on;
-
-    #keepalive_timeout  0;
-    keepalive_timeout  65;
-
-    #gzip  on;
-
-    server {
-        listen       80;
-        server_name  localhost;
-
-        #charset koi8-r;
-
-        #access_log  logs/host.access.log  main;
-
-        location / {
-            root   /usr/share/nginx;
-            index  index.html index.htm;
-        }
-
-        #error_page  404              /404.html;
-
-        # redirect server error pages to the static page /50x.html
-        #
-        error_page   500 502 503 504  /50x.html;
-        location = /50x.html {
-            root   /usr/share/nginx;
-        }
-
-        # proxy the PHP scripts to Apache listening on 127.0.0.1:80
-        #
-        #location ~ \.php$ {
-        #    proxy_pass   http://127.0.0.1;
-        #}
-
-        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-        #
-        #location ~ \.php$ {
-        #    root           html;
-        #    fastcgi_pass   127.0.0.1:9000;
-        #    fastcgi_index  index.php;
-        #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
-        #    include        fastcgi_params;
-        #}
-
-        # deny access to .htaccess files, if Apache's document root
-        # concurs with nginx's one
-        #
-        #location ~ /\.ht {
-        #    deny  all;
-        #}
-    }
-
-
-    # another virtual host using mix of IP-, name-, and port-based configuration
-    #
-    #server {
-    #    listen       8000;
-    #    listen       somename:8080;
-    #    server_name  somename  alias  another.alias;
-
-    #    location / {
-    #        root   html;
-    #        index  index.html index.htm;
-    #    }
-    #}
-
-
-    # HTTPS server
-    #
-    #server {
-    #    listen       443 ssl;
-    #    server_name  localhost;
-
-    #    ssl_certificate      cert.pem;
-    #    ssl_certificate_key  cert.key;
-
-    #    ssl_session_cache    shared:SSL:1m;
-    #    ssl_session_timeout  5m;
-
-    #    ssl_ciphers  HIGH:!aNULL:!MD5;
-    #    ssl_prefer_server_ciphers  on;
-
-    #    location / {
-    #        root   html;
-    #        index  index.html index.htm;
-    #    }
-    #}
-
-include conf.d/*.conf;
-}
-
-EOF
 
 #安装rclone
 yum install fuse -y
