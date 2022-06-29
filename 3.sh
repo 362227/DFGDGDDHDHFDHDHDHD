24dcad3c601ede3f425456c967adb04937787caf
10362227 <10362227@163.com>
2022-06-29 12:19:17 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -17,6 +17,10 @@ ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip3
 pip3 install --user --upgrade git+https://github.com/streamlink/streamlink.git
 cp /root/.local/bin/streamlink /usr/local/bin
 
+#安装gdown
+pip3 install --user --upgrade git+https://github.com/wkentaro/gdown.git
+cp /root/.local/bin/gdown /usr/local/bin
+
 #安装v2ray
 wget -N --no-check-certificate -q -O install.sh "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh" && chmod +x install.sh && bash install.sh
 #wget -N --no-check-certificate -q -O install.sh "https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh" && chmod +x install.sh && bash install.sh
