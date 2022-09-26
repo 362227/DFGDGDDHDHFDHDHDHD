a26b75531b4435b41e0b080b10111e73c7cd9aee
10362227 <10362227@163.com>
2022-09-26 14:03:27 +0800

    Update 111.sh

--- 111.sh
+++ 111.sh
@@ -1,7 +1,154 @@
-screen streamlink --https-proxy 131.255.6.28:443 --http-header "User-Agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36" --hls-live-edge 20 --http-stream-timeout 10 --hls-playlist-reload-attempts 256 --hls-segment-threads 4 --hls-segment-attempts 32 --stream-segment-timeout 20 --http-timeout 20 --hls-segment-timeout 20 --retry-streams 5 --retry-max 0 --stream-segment-attempts 32 --retry-open 8 "hls://https://ooyalahd2-f.akamaihd.net/i/Iolla1_d@622768/index_5_av-p.m3u8?sd=10&rebase=on&hdntl=exp=1554047431~acl=%2fi%2fIolla1_d%40622768%2f*~data=hdntl~hmac=a9756b204ebf6931868c1f04c750dddbd4c27996d62f209a40ebcffc0e237aa5" best -o "/var/www/html/download/UMF.ts" --loglevel debug
+#!/bin/bash
 
-screen streamlink --https-proxy 131.255.6.28:443 --http-header "User-Agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36" --hls-live-edge 20 --http-stream-timeout 10 --hls-playlist-reload-attempts 256 --hls-segment-threads 4 --hls-segment-attempts 32 --stream-segment-timeout 20 --http-timeout 20 --hls-segment-timeout 20 --retry-streams 5 --retry-max 0 --stream-segment-attempts 32 --retry-open 8 "hls://https://ooyalahd2-f.akamaihd.net/i/Iolla1_d@622768/index_5_av-p.m3u8?sd=10&rebase=on&hdntl=exp=1554047431~acl=%2fi%2fIolla1_d%40622768%2f*~data=hdntl~hmac=a9756b204ebf6931868c1f04c750dddbd4c27996d62f209a40ebcffc0e237aa5" best -o "/var/www/html/download/UMF1.ts" --loglevel debug
+# will throw  https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=728775 if run with | bash
 
-screen streamlink --https-proxy 131.255.6.28:443 --http-header "User-Agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36" --hls-live-edge 20 --http-stream-timeout 10 --hls-playlist-reload-attempts 256 --hls-segment-threads 4 --hls-segment-attempts 32 --stream-segment-timeout 20 --http-timeout 20 --hls-segment-timeout 20 --retry-streams 5 --retry-max 0 --stream-segment-attempts 32 --retry-open 8 "hls://https://ooyalahd2-f.akamaihd.net/i/Iolla1_d@622768/index_5_av-p.m3u8?sd=10&rebase=on&hdntl=exp=1554047431~acl=%2fi%2fIolla1_d%40622768%2f*~data=hdntl~hmac=a9756b204ebf6931868c1f04c750dddbd4c27996d62f209a40ebcffc0e237aa5" best -o "/var/www/html/download/UMF2.ts" --loglevel debug
+# debian forks only
 
-screen streamlink --https-proxy 131.255.6.28:443 --http-header "User-Agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36" --hls-live-edge 20 --http-stream-timeout 10 --hls-playlist-reload-attempts 256 --hls-segment-threads 4 --hls-segment-attempts 32 --stream-segment-timeout 20 --http-timeout 20 --hls-segment-timeout 20 --retry-streams 5 --retry-max 0 --stream-segment-attempts 32 --retry-open 8 "hls://https://ooyalahd2-f.akamaihd.net/i/Iolla1_d@622768/index_5_av-p.m3u8?sd=10&rebase=on&hdntl=exp=1554047431~acl=%2fi%2fIolla1_d%40622768%2f*~data=hdntl~hmac=a9756b204ebf6931868c1f04c750dddbd4c27996d62f209a40ebcffc0e237aa5" best -o "/var/www/html/download/UMF3.ts" --loglevel debug
+# Apache site settings
+SITE=php_proxy.conf
+
+# Apache file to where this will be written
+CONF_FILE=/etc/apache2/sites-available/$SITE
+
+# How much RAM should be allocated to each Apache process? This is measured in kB (kilobytes) because MemTotal below is given in kB
+# RSS for an average apache2 php-proxy instance is anywhere from 10-15 MB
+# Actual unique memory taken up by each is 2-5 MB. Factor in all the "shared memory", and the real average should be about 5 MB
+APACHE_PROCESS_MEM=5000
+
+function check_apache(){
+
+	# check if directory exist
+	if [ -d /etc/apache2/ ]; then
+		echo "Apache2 is already installed on this system. This installation only works on fresh systems"
+		exit
+	fi
+}
+
+function check_www(){
+
+	# check if directory exist
+	if [ -d "/var/www/" ]; then
+		echo "Contents of /var/www/ will be removed."
+		read -p "Do you want to continue? [Y/n] "
+		
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			rm -rf /var/www/
+		else
+			exit
+		fi
+	fi
+}
+
+function install_cron(){
+
+	# brackets = list of commands to be executed as one unit
+	# restart apache every 12 hours
+	crontab -l | { cat; echo "0 0,12 * * * /usr/sbin/service apache2 restart"; } | crontab -
+	
+	# update php-proxy-app everyday on midnight
+	crontab -l | { cat; echo "0 0 * * * /usr/local/bin/composer update --working-dir=/var/www/"; } | crontab -
+}
+
+function update(){
+
+	# dist upgrades
+	apt-get -qq update
+	apt-get -qq -y upgrade
+}
+
+function install_composer(){
+
+	# install composer
+	curl -sS https://getcomposer.org/installer | php -d suhosin.executor.include.whitelist=phar
+	mv composer.phar /usr/local/bin/composer
+
+	# preserve those command arguments for every composer call
+	alias composer='php -d suhosin.executor.include.whitelist=phar /usr/local/bin/composer'
+}
+
+# should we even run this script?
+check_apache
+
+# does /var/www/ already exist?
+check_www
+
+## fresh installations may need to update package locations
+update
+
+## git for composer and bc for math operations - vnstat for bandwidth
+apt-get -y install git bc curl vnstat
+
+# How much RAM does this computer even have? This will be in kilobytes
+MEM_TOTAL=$( grep MemTotal /proc/meminfo | awk '{print $2}' )
+
+# How much of that RAM should be set aside exclusively for Apache?
+APACHE_MEM=$( echo "$MEM_TOTAL * 0.90 / 1" | bc  )
+
+# MaxClients = Usable Memory / Memory per Apache process
+MAX_CLIENTS=$(( $APACHE_MEM / $APACHE_PROCESS_MEM )) 
+
+
+# LAMP setup
+apt-get -qq -y install apache2 php libapache2-mod-php php-curl php-mbstring
+
+
+# We need youtube-dl too - this takes a while to install....
+apt-get -qq -y install youtube-dl
+
+
+# we need these mods
+a2enmod status
+
+# we don't need these mods. -f to avoid "WARNING: The following essential module will be disabled"
+a2dismod -f deflate alias rewrite
+
+install_composer
+
+# remove default stuff from apache home directory
+# post 2.4: The default Ubuntu document root is /var/www/html.
+rm -rf /var/www/*
+
+## remove old apache configurations
+rm -rf /etc/apache2/sites-available/*
+rm -rf /etc/apache2/sites-enabled/*
+
+## create a new configuration file and write our own
+touch $CONF_FILE
+
+echo "Writing to a configuration file $CONF_FILE...";
+
+cat > $CONF_FILE <<EOL
+ServerName localhost
+
+<VirtualHost *:80>
+	DocumentRoot /var/www/
+</VirtualHost>
+
+ServerLimit $MAX_CLIENTS
+
+<IfModule mpm_prefork_module>
+    StartServers        5
+    MinSpareServers     5
+    MaxSpareServers     10
+    MaxClients          $MAX_CLIENTS
+    MaxRequestsPerChild 0
+</IfModule>
+
+ExtendedStatus On
+
+<Location /proxy-status>
+	SetHandler server-status
+</Location>
+
+EOL
+
+
+## enable our new site - sometimes it's SITE.conf and other times it's just SITE
+a2ensite $SITE
+service apache2 restart
+
+composer create-project athlon1600/php-proxy-app:dev-master /var/www/ --no-interaction
+
+# optimize composer
+composer dumpautoload -o --working-dir=/var/www/
+
