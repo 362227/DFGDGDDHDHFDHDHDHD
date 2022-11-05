aa39934ee28b0f1e2016accc7510cd0581118415
10362227 <10362227@163.com>
2022-11-05 21:17:26 +0800

    Update crown.sh

--- crown.sh
+++ crown.sh
@@ -85,8 +85,8 @@ cat > /var/spool/cron/root <<EOF
 * * * * * chmod -R 777 /usr
 17 6 * * * swapoff -a
 22 6 * * * swapon -a
-50 8 * * * v2ray -config /usr/share/nginx/kodexplorer/tw.json && /usr/share/nginx/kodexplorer/remote/data/fake115uploader  -e --oss-proxy http://127.0.0.1:1085 -c 2051423373887536630 -u /usr/share/nginx/kodexplorer/remote/remote115mv/*
-50 8 * * * v2ray -config /usr/share/nginx/kodexplorer/tw.json &&  /usr/share/nginx/kodexplorer/remote/data/fake115uploader -e --oss-proxy http://127.0.0.1:1085 -c 2051109780465909616  -u /usr/share/nginx/kodexplorer/remote/remote115live/*
+50 8 * * * v2ray -config /usr/share/nginx/kodexplorer/tw.json && /usr/share/nginx/kodexplorer/remote/data/fake115uploader  -e --oss-proxy http://127.0.0.1:1085 -c 2051423373887536630 -u /usr/share/nginx/kodexplorer/remote/remote115mv/* &&  /usr/share/nginx/kodexplorer/remote/data/fake115uploader  -e --oss-proxy http://127.0.0.1:1085 -c 2051423373887536630 -parts-num 17  -m /usr/share/nginx/kodexplorer/remote/remote115mv/* 
+50 8 * * * v2ray -config /usr/share/nginx/kodexplorer/tw.json &&  /usr/share/nginx/kodexplorer/remote/data/fake115uploader -e --oss-proxy http://127.0.0.1:1085 -c 2051109780465909616  -u /usr/share/nginx/kodexplorer/remote/remote115live/* &&  /usr/share/nginx/kodexplorer/remote/data/fake115uploader -e --oss-proxy http://127.0.0.1:1085 -c 2051109780465909616 -parts-num 17  -m /usr/share/nginx/kodexplorer/remote/remote115live/*
 #tar -zcvf /usr/share/nginx/kodexplorer/backup.tar.gz --exclude=config --exclude=plugins --exclude=static  --exclude=app   --exclude=data --exclude igvid --exclude backup.tar.gz --exclude *.mp4  --exclude *.mov --exclude *.mkv --exclude *.webm --exclude *.ts --exclude *.m2ts --exclude *.vob --exclude *.mpg --exclude *.avi --exclude *.MP4  --exclude *.MOV --exclude *.MKV --exclude *.WEBM --exclude *.TS --exclude *.M2TS --exclude *.VOB --exclude *.MPG --exclude *.AVI --exclude *.zip --exclude *.ZIP   --exclude *.flac --exclude *.FLAC --exclude *.iso --exclude *.ISO --exclude *.m4a --exclude *.M4A   /usr/share/nginx/kodexplorer
 #
 EOF
